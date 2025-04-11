f09de0ed6966404992b26aa1d848c4af734b2dc7
ginny169 <tongdong13@gmail.com>
2025-04-11 20:29:49 +0700

    binary search

---
+++ src/main/java/org/example/java324/search/SqrtI.java
@@ -0,0 +1,32 @@
+package org.example.java324.search;
+
+public class SqrtI {
+    public static void main(String[] args) {
+        int n = 9;
+        System.out.println(mySqrt(n));
+
+    }
+
+    public static int mySqrt(int x) {
+        if (x < 2) {
+            return x;
+        }
+        int result = 0;
+        int left = 1;
+        int right = x;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long s = (long) mid * mid;
+            if (s == x) {
+                return mid;
+            } else if (s > x) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return result - 1;
+    }
+
+}
